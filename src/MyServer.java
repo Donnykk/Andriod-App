9169376e6fffd0d0f49b9dd8d7ab24785ebf0fe2
Donny <tpc1789883718@163.com>
2022-08-12 19:56:42 +0800

    EOFExceptio初步解决

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -16,71 +11,103 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MyServer {
-    private ServerSocket server;
 
-    public MyServer() {
-    }
+    private ServerSocket server;
 
     public void run(String[] args) {
         int port = 8088;
         String endpoint = "localhost:" + port;
         try {
             this.server = new ServerSocket(port);
-        } catch (IOException var5) {
+        } catch (IOException e) {
             System.err.println("服务开启失败 => " + endpoint);
-            var5.printStackTrace();
+            e.printStackTrace();
             return;
         }
         System.out.println("服务开启成功 => " + endpoint);
         while (true) {
-            this.doLoop();
+            doLoop();
+        }
+    }
+
+    private void register() {
+        Socket client = null;
+        DataInputStream dis = null;
+        try {
+            client = server.accept();
+            dis = new DataInputStream(client.getInputStream());
+            String cmd = dis.readUTF();
+            // 针对有人注册没注册完就退出了程序然后另外有人开始登陆或者重新注册
+            System.out.println("运行命令 => " + cmd);
+            String[] cmds = cmd.split("\\s+");
+            if (cmds.length < 3) {
+                System.err.println("命令长度异常 => " + cmd);
+                return;
+            }
+            switch (cmds[2]) {
+                case "Login":
+                    login(client, cmds);
+                    break;
+                case "Register":
+                    break;
+                case "Registered":
+                    insertMasterDB(cmds);
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            close(dis, client);
+        }
+    }
+
+    private static void close(DataInputStream dis, Socket socket) {
+        try {
+            if (dis != null) {
+                dis.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     private void doLoop() {
-        System.out.println("服务器起始点");
-
+        System.out.println("服务器起始点"); //调试信息
+        Socket client = null;
+        DataInputStream dis = null;
         try {
-            System.out.println("接到客户端socket请求");
-            Socket client = this.server.accept();
-            DataInputStream dis = new DataInputStream(client.getInputStream());
-            String cmd1 = dis.readUTF();
-            System.out.println("运行命令 => " + cmd1);
-            String[] cmd1s = cmd1.split("\\s+");
-            switch (cmd1s[2]) {
+            System.out.println("接到客户端socket请求"); // 调试信息
+            client = server.accept();
+            dis = new DataInputStream(client.getInputStream());
+            String cmd = dis.readUTF();
+            System.out.println("运行命令 => " + cmd);
+            String[] cmds = cmd.split("\\s+"); // 用空格把账号和密码分开存储
+            if (cmds.length < 3) {
+                System.err.println("命令长度异常 => " + cmd);
+                return;
+            }
+            switch (cmds[2]) {
                 case "Register":
                     System.out.println("等待下一步指令");
-                    Socket s2 = this.server.accept();
-                    DataInputStream dis2 = new DataInputStream(s2.getInputStream());
-                    String cmd2 = dis2.readUTF();
-                    System.out.println(cmd2);
-                    String[] cmd2s = cmd2.split("\\s+");
-                    switch (cmd2s[2]) {
-                        case "Login":
-                            login(s2, cmd2s);
-                        case "Register":
-                        default:
-                            break;
-                        case "Registered":
-                            insertMasterDB(cmd2s);
-                    }
-
-                    s2.close();
-                    dis2.close();
+                    register();
                     break;
+                // 如果发来登录账号+密码+login做如下操作
                 case "Login":
                     System.out.println("进入登陆判断");
-                    login(client, cmd1s);
+                    login(client, cmds);
                     break;
                 case "Registered":
-                    insertMasterDB(cmd1s);
+                    insertMasterDB(cmds);
+                    break;
             }
-            dis.close();
-            client.close();
-        } catch (IOException var13) {
-            var13.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            close(dis, client);
         }
-
     }
 
     public static void main(String[] args) {
@@ -91,64 +118,75 @@ public class MyServer {
     private static void insertMasterDB(String[] commands) {
         ServerDatabase masterDB = new ServerDatabase();
         masterDB.connectSQL();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 定义要输出日期字符串的格式
         Date startTime = new Date();
         String startTimeSting = sdf.format(startTime);
-        String s = "select * from userdata";
-        String insert = "insert into userdata(username,password,time) values('" + commands[0] + "','" + MD5(commands[1]) + "','" + startTimeSting + "')";
+        String s = "select * from userdata";// 调试信息
+        String insert = "insert into userdata(username,password,time) " + "values('" + commands[0] + "','" + MD5(commands[1]) + "','" + startTimeSting + "')";
         if (masterDB.insertSQL(insert)) {
             System.out.println("insert successfully");
-            ResultSet resultSet = masterDB.selectSQL(s);
-            masterDB.layoutStyle2(resultSet);
+            ResultSet resultSet = masterDB.selectSQL(s);// 调试信息
+            masterDB.layoutStyle2(resultSet);// 调试信息
         }
-        masterDB.deconnectSQL();
+        masterDB.deconnectSQL();// 关闭连接
     }
 
+    /**
+     * 该函数为是否允许用户登录函数
+     *
+     * @param s1  建立连接的socket变量
+     * @param str 要查找的username和password都在里面
+     */
     public static void login(Socket s1, String[] str) {
         ServerDatabase masterDB = new ServerDatabase();
         masterDB.connectSQL();
         String select = "select * from userdata where username = '" + str[0] + "' and password = '" + MD5(str[1]) + "';";
         System.out.println(select);
         ResultSet resultSet = masterDB.selectSQL(select);
+        // ///////////////查找用户名和密码是否一致//////////////
         try {
-            OutputStream os;
-            DataOutputStream dos;
+            // 用户名和密码不一致
             if (!resultSet.next()) {
-                os = s1.getOutputStream();
-                dos = new DataOutputStream(os);
+                // 禁止登录命令
+                OutputStream os = s1.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(os);
                 dos.writeUTF("NO");
                 dos.close();
                 System.out.println("用户密码错误");
-            } else {
-                os = s1.getOutputStream();
-                dos = new DataOutputStream(os);
+            }
+            // 用户名和密码一致
+            else {
+                // 允许登录命令
+                OutputStream os = s1.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(os);
                 dos.writeUTF("YES");
                 dos.close();
-                System.out.println("用户密码正确");
+                System.out.println("用户密码正确");// 调试信息
             }
-        } catch (Exception var7) {
+        } catch (Exception e) {
             System.out.println("显示出错");
-            var7.printStackTrace();
+            e.printStackTrace();
         }
-        masterDB.deconnectSQL();
+        masterDB.deconnectSQL();// 关闭连接
     }
 
+    // MD5加密
     private static String MD5(String s) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] bytes = md.digest(s.getBytes(StandardCharsets.UTF_8));
             return toHex(bytes);
-        } catch (Exception var3) {
-            throw new RuntimeException(var3);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     private static String toHex(byte[] bytes) {
-        char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
+        final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
         StringBuilder ret = new StringBuilder(bytes.length * 2);
         for (byte aByte : bytes) {
-            ret.append(HEX_DIGITS[aByte >> 4 & 15]);
-            ret.append(HEX_DIGITS[aByte & 15]);
+            ret.append(HEX_DIGITS[(aByte >> 4) & 0x0f]);
+            ret.append(HEX_DIGITS[aByte & 0x0f]);
         }
         return ret.toString();
     }
